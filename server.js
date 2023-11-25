e4db2fa2e2d13ec290f94001a7d50f44d5a4f646
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2023-11-25 23:24:30 +0000

    added cors access|

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if ('OPTIONS' == req.method) {
+       res.sendStatus(200);
+     }
+     else {
+       next();
+    }
+});
+
 app.post("/form", (req, res) => {
     const {
         name,
@@ -16,6 +28,7 @@ app.post("/form", (req, res) => {
     res.status(200).send("Form submitted successfully");
     console.log(req.body);
 })
+
 
 app.listen(5001, () => {
     console.log("Server started on port 5001");
